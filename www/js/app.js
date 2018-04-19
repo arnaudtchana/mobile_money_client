caa8c21a731346f5c78961601fed0cd3c9179e76
arnaudtchana <arnaudtchana1994@gmail.com>
2018-04-19 18:10:56 +0100

    ajout du fichier app dot js sur github

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 //var ApiUrl = 'http://localhost/mobile_money_server/public/api';
-var ApiUrl = 'http://localhost/admin_test/public/api';
+var ApiUrl = 'http://localhost/mobile_money_admin/public/api';
  var app=angular.module('starter', ['ionic','ngCordova','restangular'])
 
 .run(function($ionicPlatform) {
